3195fe837a6fd4b6e7ad4e975eb162b9c38124db
Aaditya Padte <aadityajp0419@gmail.com>
2023-10-30 00:16:07 +0530

    get-organization-api-done-service-str-added

--- controllers/orgController.js
+++ controllers/orgController.js
@@ -1,92 +1,52 @@
 const asyncHandler = require("express-async-handler");
 const { errorResponse, successResponse } = require("../helpers/apiResponse");
-const Org = require("../models/orgModel");
-const randomstring = require("randomstring");
-const User = require("../models/userModel");
+const { create, join, fetch } = require("../services/orgService");
 
 const createOrg = asyncHandler(async (req, res) => {
-  try {
-    const { admin_id, name, image } = req.body;
-
-    if (!admin_id || !name || !image) {
-      errorResponse({ res, message: "Please fill required fields!" });
-    }
-    const orgExists = await Org.findOne({ name });
-
-    if (orgExists)
-      return errorResponse({
-        res,
-        message: "Organization with same name already exists!",
-      });
-
-    const org = await Org.create({
-      admin_id,
-      name,
-      org_code: randomstring.generate(7),
-      image,
+  const data = await create(req, res);
+  if (data) {
+    successResponse({
+      res,
+      message: "Organization created successfully",
+      data: data,
     });
-
-    if (org) {
-      const data = {
-        admin_id: org.admin_id,
-        name: org.name,
-        org_code: org.org_code,
-        image: org.image,
-      };
-      successResponse({
-        res,
-        message: "Organization created successfully",
-        data: data,
-      });
-    } else {
-      errorResponse({
-        res,
-        message: "Something went wrong! Unable to create organization",
-      });
-    }
-  } catch (error) {
-    console.log(error);
-    errorResponse({ res, message: "Something went wrong!" });
+  } else {
+    errorResponse({
+      res,
+      message: "Something went wrong! Unable to create organization",
+    });
   }
 });
+
+const getOrg = asyncHandler(async(req,res) =>{
+  const data = await fetch(req,res);
+  if(data){
+    res.status(200).send(data);
+  }else{
+    errorResponse({
+      res,
+      message: "Something went wrong! Unable to fetch organizations",
+    })
+  }
+})
 
 const joinOrg = asyncHandler(async (req, res) => {
-  try {
-    const { userId, org_code } = req.body;
+  const data = await join(req, res);
 
-    if (!userId) {
-      errorResponse({ res, message: "Please fill required fields!" });
-    }
-
-    const org = await Org.findOne({ org_code: org_code });
-
-    if (org) {
-      //Storing id of the user to Org collection
-      org.users.push(userId);
-      await org.save(); // updating document
-
-      //Linking user with org
-      const user = await User.findOne({ _id: userId });
-      user.org_joined = org.slug;
-      await user.save();
-
-      const userData = await org.populate("users"); // retrieving respective users data using populate
-      
-      successResponse({
-        res,
-        message: "Organization joined successfully",
-        data: userData,
-      });
-    } else {
-      errorResponse({
-        res,
-        message: "Oraganization doesn't exist",
-      });
-    }
-  } catch (error) {
-    console.log(error);
-    errorResponse({ res, message: "Something went wrong!" });
+  if (data) {
+    successResponse({
+      res,
+      message: "Organization joined successfully",
+      data: data,
+    });
+  } else {
+    errorResponse({
+      res,
+      message: "Please enter valid code.Oraganization doesn't exist!!",
+    });
   }
 });
 
-module.exports = { createOrg, joinOrg };
+
+
+module.exports = { createOrg, joinOrg,getOrg };
--- routes/orgRoutes.js
+++ routes/orgRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
-const { createOrg,joinOrg } = require("../controllers/orgController");
+const { createOrg,joinOrg, getOrg } = require("../controllers/orgController");
 const protect = require('../middlewares/authMiddleware');
 
 
@@ -9,5 +9,6 @@ const protect = require('../middlewares/authMiddleware');
 
 router.route("/create-org").post(protect,createOrg);
 router.route("/join-org").post(protect,joinOrg);
+router.route("/get-org").get(protect,getOrg);
 
 module.exports = router;
