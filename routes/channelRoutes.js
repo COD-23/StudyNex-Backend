b3ea05af6d82d9432dbf13959c4b81c9b64d9db2
Aaditya Padte <aadityajp0419@gmail.com>
2023-11-10 15:36:28 +0530

    rename_api_renamed

--- routes/channelRoutes.js
+++ routes/channelRoutes.js
@@ -15,7 +15,7 @@ const router = express.Router();
 router.route("/create-channel").post(protect, createChannel);
 router.route("/join-channel").post(protect, joinChannel);
 router.route("/fetch-all-channels").get(protect, fetchAllChannels);
-router.route("/rename").put(protect, renameChannel);
+router.route("/rename-channel").put(protect, renameChannel);
 router.route("/fetch-channel/:channelId").get(protect, fetchOneChannel);
 router.route("/get-members").post(protect, getMembers);
 router.route("/channel-list").get(protect, channelList);
